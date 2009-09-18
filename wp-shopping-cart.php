b45470353055e2c0a2f0f68cb0d5373f19e65474
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-18 05:07:49 +0000

    uploading 3.7.5 Beta 2
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/trunk@155932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-shopping-cart.php
+++ wp-shopping-cart.php
@@ -3,7 +3,7 @@
 Plugin Name:WP Shopping Cart
 Plugin URI: http://www.instinct.co.nz
 Description: A plugin that provides a WordPress Shopping Cart. Contact <a href='http://www.instinct.co.nz/?p=16#support'>Instinct Entertainment</a> for support.
-Version: 3.7.5 Beta 1
+Version: 3.7.5 Beta 2
 Author: Instinct Entertainment
 Author URI: http://www.instinct.co.nz/e-commerce/
 */
@@ -14,9 +14,9 @@ Author URI: http://www.instinct.co.nz/e-commerce/
 // this is to make sure it sets up the table name constants correctly on activation
 global $wpdb;
 define('WPSC_VERSION', '3.7');
-define('WPSC_MINOR_VERSION', '34');
+define('WPSC_MINOR_VERSION', '35');
 
-define('WPSC_PRESENTABLE_VERSION', '3.7.5 Beta 1');
+define('WPSC_PRESENTABLE_VERSION', '3.7.5 Beta 2');
 
 define('WPSC_DEBUG', false);
 define('WPSC_GATEWAY_DEBUG', false);
