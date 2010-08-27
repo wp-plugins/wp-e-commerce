7f0fbc6b4ce1931ffe9a2fc3b2b5a80b14727d8f
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-27 20:50:39 +0000

    Allow images to be hidden in latest product widget
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@281708 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets/latest_product_widget.php
+++ widgets/latest_product_widget.php
@@ -42,7 +42,7 @@ class WP_Widget_Latest_Products extends WP_Widget {
 		if ( $title ) {
 			echo $before_title . $title . $after_title;
 		}
-		nzshpcrt_latest_product( array( 'number' => $instance['number'] ) );
+		nzshpcrt_latest_product( &$args, &$instance );
 		echo $after_widget;
 	
 	}
@@ -60,6 +60,7 @@ class WP_Widget_Latest_Products extends WP_Widget {
 		$instance = $old_instance;
 		$instance['title']  = strip_tags( $new_instance['title'] );
 		$instance['number'] = (int)$new_instance['number'];
+		$instance['hide_thumbnails'] = (bool)$new_instance['hide_thumbnails'];
 
 		return $instance;
 		
@@ -80,6 +81,7 @@ class WP_Widget_Latest_Products extends WP_Widget {
 		// Values
 		$title  = esc_attr( $instance['title'] );
 		$number = (int)$instance['number'];
+		$hide_thumbnails = (bool)$instance['hide_thumbnails'];
 		
 		?>
 		<p>
@@ -97,6 +99,10 @@ class WP_Widget_Latest_Products extends WP_Widget {
 				}
 				?>
 			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'hide_thumbnails' ); ?>"><?php _e( 'Hide Thumbnails', 'wpsc' ); ?></label>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'hide_thumbnails' ); ?>" name="<?php echo $this->get_field_name( 'hide_thumbnails' ); ?>" <?php echo $hide_thumbnails ? 'checked="checked"' : ""; ?>>
 		</p>
 		<?php
 	}
@@ -120,9 +126,9 @@ add_action( 'widgets_init', create_function( '', 'return register_widget("WP_Wid
  * 2. <br /> tags have been ommitted. Padding and margins should be applied via css.
  * 3. Each product is enclosed in a <div> with a 'wpec-latest-product' class.
  * 4. The product list is enclosed in a <div> with a 'wpec-latest-products' class.
- * 5. Function now expect a single paramter with an array of options (used to be a string which prepended the output).
+ * 5. Function now expects two arrays as per the standard Widget API.
  */
-function nzshpcrt_latest_product( $args = null ) {
+function nzshpcrt_latest_product( $args = null, $instance ) {
 	
 	global $wpdb;
 	
@@ -130,7 +136,8 @@ function nzshpcrt_latest_product( $args = null ) {
 	
 	$siteurl = get_option( 'siteurl' );
 	$options = get_option( 'wpsc-widget_latest_products' );
-	$number  = (int)$args['number'];
+	$number  = isset($instance['number'] ? (int)$instance['number'] : 5;
+	$hide_thumbnails  = isset($instance['hide_thumbnails'] ? (bool)$instance['hide_thumbnails'] : FALSE;
 	
 	$latest_products = get_posts( array(
 		'post_type'   => 'wpsc-product',
@@ -148,29 +155,31 @@ function nzshpcrt_latest_product( $args = null ) {
 		foreach ( $latest_products as $latest_product ) {
 			$output .= '<div class="wpec-latest-product">';
 			
-			// Image
-			$output .= '<div class="item_image">';
-			$output .= '<a href="' . wpsc_product_url( $latest_product->ID, null ) . '">';
-			$attached_images = (array)get_posts( array(
-				'post_type'   => 'attachment',
-				'numberposts' => 1,
-				'post_status' => null,
-				'post_parent' => $latest_product->ID,
-				'orderby'     => 'menu_order',
-				'order'       => 'ASC'
-			) );
-			$attached_image = $attached_images[0]; 
-			if ( $attached_image->ID > 0 ) {
-				if ( get_option( 'wpsc_selected_theme' ) == 'marketplace' ) {
-					$src = WPSC_IMAGE_URL . $special['image'];
-					$output .= '<img src="' . wpsc_product_image( $attached_image->ID, 100, 75 ) . '" title="' . $latest_product->post_title . '" alt="' . $latest_product->post_title . '" />';
-				} else {
-					$output .= '<img src="' . wpsc_product_image( $attached_image->ID, 45, 25 ) . '" title="' . $latest_product->post_title . '" alt="' . $latest_product->post_title . '" />';
+			// Thumbnails, if required
+			if (!$hide_thumbnails) {
+				$output .= '<div class="item_image">';
+				$output .= '<a href="' . wpsc_product_url( $latest_product->ID, null ) . '">';
+				$attached_images = (array)get_posts( array(
+					'post_type'   => 'attachment',
+					'numberposts' => 1,
+					'post_status' => null,
+					'post_parent' => $latest_product->ID,
+					'orderby'     => 'menu_order',
+					'order'       => 'ASC'
+				) );
+				$attached_image = $attached_images[0]; 
+				if ( $attached_image->ID > 0 ) {
+					if ( get_option( 'wpsc_selected_theme' ) == 'marketplace' ) {
+						$src = WPSC_IMAGE_URL . $special['image'];
+						$output .= '<img src="' . wpsc_product_image( $attached_image->ID, 100, 75 ) . '" title="' . $latest_product->post_title . '" alt="' . $latest_product->post_title . '" />';
+					} else {
+						$output .= '<img src="' . wpsc_product_image( $attached_image->ID, 45, 25 ) . '" title="' . $latest_product->post_title . '" alt="' . $latest_product->post_title . '" />';
+					}
 				}
+				$output .= '</a>';
+				$output .= '</div>';
 			}
-			$output .= '</a>';
-			$output .= '</div>';
-			
+				
 			// Link
 			$output .= '<a href="' . wpsc_product_url( $latest_product->ID, null ) . '" class="wpec-product-title">';
 			$output .= stripslashes( $latest_product->post_title );
