6077f49608a643736cdd5854a3091a0c118f62a2
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-13 22:48:27 +0000

    Revert N/A return
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@289012 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-includes/processing.functions.php
+++ wpsc-includes/processing.functions.php
@@ -11,10 +11,6 @@
 function wpsc_currency_display( $price_in, $args ) {
 	global $wpdb, $wpsc_currency_data;
 
-	// If price_in isn't
-	if ( empty( $price ) || !is_double( $price_in ) )
-		return __( 'N/A', 'wpsc' );
-
 	// Default variables
 	$price_out     = null;
 	$currency_sign = '';
