ab865630d3f85b00faf1414aa0bc968c4afd5dca
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-16 03:00:19 +0000

    Fixed missing or incorrect domain GetText Calls : http://code.google.com/p/wp-e-commerce/issues/detail?id=63
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@290070 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/tinymce3/window.php
+++ js/tinymce3/window.php
@@ -21,9 +21,9 @@ global $wpdb; ?>
 		<form name="WPSC" action="#">
 			<div class="tabs">
 				<ul>
-					<li id="category" class="current"><span><a href="javascript:mcTabs.displayTab('category','wpsc_category_panel');" onmousedown="return false;"><?php _e("Category", 'wpsc_category'); ?></a></span></li>
-					<li id="product_slider"><span><a href="javascript:mcTabs.displayTab('product_slider','product_slider_panel');" onmousedown="return false;"><?php _e("Product Slider", 'wpsc_category'); ?></a></span></li>
-					<li id="add_product"><span><a href="javascript:mcTabs.displayTab('add_product','add_product_panel');" onmousedown="return false;"><?php _e("Add Product", 'wpsc_category'); ?></a></span></li>
+					<li id="category" class="current"><span><a href="javascript:mcTabs.displayTab('category','wpsc_category_panel');" onmousedown="return false;"><?php _e("Category", 'wpsc'); ?></a></span></li>
+					<li id="product_slider"><span><a href="javascript:mcTabs.displayTab('product_slider','product_slider_panel');" onmousedown="return false;"><?php _e("Product Slider", 'wpsc'); ?></a></span></li>
+					<li id="add_product"><span><a href="javascript:mcTabs.displayTab('add_product','add_product_panel');" onmousedown="return false;"><?php _e("Add Product", 'wpsc'); ?></a></span></li>
 				</ul>
 			</div>
 			<div class="panel_wrapper">
@@ -31,10 +31,10 @@ global $wpdb; ?>
 					<br />
 					<table border="0" cellpadding="4" cellspacing="0">
 						<tr>
-							<td nowrap="nowrap"><label for="wpsc_category"><?php _e("Select Category", 'wpsc_category'); ?></label></td>
+							<td nowrap="nowrap"><label for="wpsc_category"><?php _e("Select Category", 'wpsc'); ?></label></td>
 							<td>
 								<select id="wpsc_category" name="wpsc_category" style="width: 150px">
-									<option value="0"><?php _e("No Category", 'wpsc_category'); ?></option>
+									<option value="0"><?php _e("No Category", 'wpsc'); ?></option>
 <?php
 $categorylist = $wpdb->get_results("SELECT * FROM ".WPSC_TABLE_PRODUCT_CATEGORIES." WHERE active = '1' ORDER BY id ASC",ARRAY_A);
 if(is_array($categorylist)) {
@@ -46,7 +46,7 @@ if(is_array($categorylist)) {
 							</td>
 						</tr>
 						<tr>
-							<td nowrap="nowrap" valign="top"><label for="wpsc_perpage"><?php _e("Number of items per Page", 'wpsc_category'); ?></label></td>
+							<td nowrap="nowrap" valign="top"><label for="wpsc_perpage"><?php _e("Number of items per Page", 'wpsc'); ?></label></td>
 							<td><input name="number_per_page" id="wpsc_perpage" type="text" value="6" style="width: 80px" /></td>
 						</tr>
 					</table>
@@ -55,10 +55,10 @@ if(is_array($categorylist)) {
 					<br />
 					<table border="0" cellpadding="4" cellspacing="0">
 						<tr>
-							<td nowrap="nowrap"><label for="wpsc_category"><?php _e("Select Category", 'wpsc_category'); ?></label></td>
+							<td nowrap="nowrap"><label for="wpsc_category"><?php _e("Select Category", 'wpsc'); ?></label></td>
 							<td>
 								<select id="wpsc_slider_category" name="wpsc_category" style="width: 200px">
-									<option value="0"><?php _e("No Category", 'wpsc_category'); ?></option>
+									<option value="0"><?php _e("No Category", 'wpsc'); ?></option>
 <?php
 $categorylist = $wpdb->get_results("SELECT * FROM ".WPSC_TABLE_PRODUCT_CATEGORIES." WHERE active = '1' ORDER BY id ASC",ARRAY_A);
 if(is_array($categorylist)) {
@@ -70,7 +70,7 @@ if(is_array($categorylist)) {
 							</td>
 						</tr>
 						<tr>
-							<td><?php _e("Number of visible items", 'wpsc_category'); ?>:</td>
+							<td><?php _e("Number of visible items", 'wpsc'); ?>:</td>
 							<td>
 								<input type="text" id="wpsc_slider_visibles" name="wpsc_slider_visibles" />
 							</td>
@@ -81,22 +81,22 @@ if(is_array($categorylist)) {
 					<br />
 					<table border="0" cellpadding="4" cellspacing="0">
 						<tr>
-							<td nowrap="nowrap"><label for="add_product_name"><?php _e("Name", 'wpsc_category'); ?></label></td>
+							<td nowrap="nowrap"><label for="add_product_name"><?php _e("Name", 'wpsc'); ?></label></td>
 							<td><input type="text" id="add_product_name" name="add_product_name" style="width: 200px"></td>
 						</tr>
 						<tr>
-							<td nowrap="nowrap"><label for="add_product_description"><?php _e("Description", 'wpsc_category'); ?></label></td>
+							<td nowrap="nowrap"><label for="add_product_description"><?php _e("Description", 'wpsc'); ?></label></td>
 							<td><input type="text" id="add_product_description" name="add_product_description" style="width: 200px"></td>
 						</tr>
 						<tr>
-							<td nowrap="nowrap"><label for="add_product_price"><?php _e("Price", 'wpsc_category'); ?></label></td>
+							<td nowrap="nowrap"><label for="add_product_price"><?php _e("Price", 'wpsc'); ?></label></td>
 							<td><input type="text" id="add_product_price" name="add_product_price" style="width: 200px"></td>
 						</tr>
 						<tr>
-							<td><label for="add_product_category"><?php _e("Category", 'wpsc_category'); ?></label></td>
+							<td><label for="add_product_category"><?php _e("Category", 'wpsc'); ?></label></td>
 							<td>
 								<select id="add_product_category" name="add_product_category" style="width: 200px">
-									<option value="0"><?php _e("No Category", 'wpsc_category'); ?></option>
+									<option value="0"><?php _e("No Category", 'wpsc'); ?></option>
 <?php
 $categorylist = $wpdb->get_results("SELECT * FROM ".WPSC_TABLE_PRODUCT_CATEGORIES." ORDER BY id ASC",ARRAY_A);
 if(is_array($categorylist)) {
@@ -112,10 +112,10 @@ if(is_array($categorylist)) {
 			</div>
 			<div class="mceActionPanel">
 				<div style="float: left">
-					<input type="button" id="cancel" name="cancel" value="<?php _e("Cancel", 'wpsc_category'); ?>" onclick="tinyMCEPopup.close();" />
+					<input type="button" id="cancel" name="cancel" value="<?php _e("Cancel", 'wpsc'); ?>" onclick="tinyMCEPopup.close();" />
 				</div>
 				<div style="float: right">
-					<input type="submit" id="insert" name="insert" value="<?php _e("Insert", 'wpsc_category'); ?>" onclick="insertWPSCLink();" />
+					<input type="submit" id="insert" name="insert" value="<?php _e("Insert", 'wpsc'); ?>" onclick="insertWPSCLink();" />
 				</div>
 			</div>
 		</form>
