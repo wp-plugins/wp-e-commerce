a71a989772ad5f6ecd7eef3a4555bee165248da8
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-20 10:46:54 +0000

    Updated donations widget to use the Widget API.
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@291605 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets/donations_widget.php
+++ widgets/donations_widget.php
@@ -1,131 +1,180 @@
 <?php
-/*
- * Special widget function, 
- * @todo make this use the new widget API
- * takes the settings, works out if there is anything to display, if so, displays it	
+
+
+
+/**
+ * Donations widget class
+ *
+ * @since 3.8
  */
-function widget_donations($args) {
-  global $wpdb, $table_prefix;
-  extract($args);
-  $options = get_option('wpsc-nzshpcrt_donations');    
-	$title = empty($options['title']) ? __(__('Product Donations', 'wpsc')) : $options['title'];
-	//$donation_count = $wpdb->get_var("SELECT COUNT(*) AS `count` FROM `".WPSC_TABLE_PRODUCT_LIST."` WHERE `donation` IN ('1') AND `active` IN ('1')");
-    $donation_count = $wpdb->get_var("SELECT COUNT(DISTINCT `p`.`ID`) AS `count`
-	FROM `".$wpdb->postmeta."` AS `m`
-	JOIN `".$wpdb->posts."` AS `p` ON `m`.`post_id` = `p`.`ID`
-	WHERE `p`.`post_parent` 
-		IN ('0')
-	AND `m`.`meta_key`
-		IN ('_wpsc_is_donation')
-	AND `m`.`meta_value` 
-		IN( '1' )
-	AND `p`.`post_status` = 'publish'");   
-	if($donation_count > 0) {
-    echo $before_widget; 
-    $full_title = $before_title . $title . $after_title;
-    echo $full_title;
+class WP_Widget_Donations extends WP_Widget {
+	
+	/**
+	 * Widget Constuctor
+	 */
+	function WP_Widget_Donations() {
+
+		$widget_ops = array(
+			'classname'   => 'widget_wpsc_donations',
+			'description' => __( 'Donations Widget', 'wpsc' )
+		);
+		
+		$this->WP_Widget( 'wpsc_donations', __( 'Product Donations', 'wpsc' ), $widget_ops );
+	
+	}
+
+	/**
+	 * Widget Output
+	 *
+	 * @param $args (array)
+	 * @param $instance (array) Widget values.
+	 *
+	 */
+	function widget( $args, $instance ) {
+		
+		global $wpdb, $table_prefix;
+		
+		extract( $args );
+		
+		$donation_count = $wpdb->get_var( "SELECT COUNT(DISTINCT `p`.`ID`) AS `count`
+			FROM `" . $wpdb->postmeta . "` AS `m`
+			JOIN `" . $wpdb->posts . "` AS `p` ON `m`.`post_id` = `p`.`ID`
+			WHERE `p`.`post_parent` IN ('0')
+				AND `m`.`meta_key` IN ('_wpsc_is_donation')
+				AND `m`.`meta_value` IN( '1' )
+				AND `p`.`post_status` = 'publish'" );   
+		
+		if ( $donation_count > 0 ) {
+			echo $before_widget; 
+			$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Product Donations' ) : $instance['title'] );
+			if ( $title ) {
+				echo $before_title . $title . $after_title;
+			}
 			nzshpcrt_donations();
-    echo $after_widget;
-    }
+			echo $after_widget;
+		}
+		
+	}
+
+	/**
+	 * Update Widget
+	 *
+	 * @param $new_instance (array) New widget values.
+	 * @param $old_instance (array) Old widget values.
+	 *
+	 * @return (array) New values.
+	 */
+	function update( $new_instance, $old_instance ) {
+	
+		$instance = $old_instance;
+		$instance['title']  = strip_tags( $new_instance['title'] );
+
+		return $instance;
+		
+	}
+
+	/**
+	 * Widget Options Form
+	 *
+	 * @param $instance (array) Widget values.
+	 */
+	function form( $instance ) {
+		
+		global $wpdb;
+		
+		// Defaults
+		$instance = wp_parse_args( (array)$instance, array(
+			'title' => __( 'Product Donations', 'wpsc' )
+		) );
+		
+		// Values
+		$title = esc_attr( $instance['title'] );
+		
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<?php
+		
+	}
+
 }
+
+add_action( 'widgets_init', create_function( '', 'return register_widget("WP_Widget_Donations");' ) );
+
 
 
 /*
  * Specials Widget content function
  * Displays the products
+ *
+ * @todo make this use wp_query and a theme file if possible
+ *
+ * Changes made in 3.8 that may affect users:
+ *
+ * 1. $input does not get prepended to output.
  */
-function nzshpcrt_donations($input = null) {
+function nzshpcrt_donations( $args = null ) {
+	
 	global $wpdb;
-	$siteurl = get_option('siteurl');
 	
-	$products = $wpdb->get_results("SELECT 
-		DISTINCT `p` . * , 
-		`m`.`meta_value` AS `special_price`
-		FROM `".$wpdb->postmeta."` AS `m`
-		JOIN `".$wpdb->posts."` AS `p` 
-		ON `m`.`post_id` = `p`.`ID`
-		WHERE `p`.`post_parent` 
-			IN ('0')
-		AND `m`.`meta_key`
-			IN ('_wpsc_is_donation')
-		AND `m`.`meta_value` 
-			IN( '1' )
+	// Args not used yet but this is ready for when it is
+	$args = wp_parse_args( (array)$args, array() );
+	
+	$siteurl = get_option( 'siteurl' );
+	
+	$products = $wpdb->get_results( "SELECT DISTINCT `p` . * , `m`.`meta_value` AS `special_price`
+		FROM `" . $wpdb->postmeta . "` AS `m`
+		JOIN `" . $wpdb->posts . "` AS `p` ON `m`.`post_id` = `p`.`ID`
+		WHERE `p`.`post_parent` IN ('0')
+			AND `m`.`meta_key` IN ('_wpsc_is_donation')
+			AND `m`.`meta_value` IN( '1' )
 		ORDER BY RAND( )
-		LIMIT 1", ARRAY_A) ;
-//	exit('<pre>'.print_r($products,true).'</pre>');
+		LIMIT 1", ARRAY_A );
 	
-
-	if($products != null) {
+	if ( $products != null ) {
 		$output = "<div><div>";
-		foreach($products as $product) {
-		 	$attached_images = (array)get_posts(array(
-				'post_type' => 'attachment',
+		foreach ( $products as $product ) {
+			$attached_images = (array)get_posts( array(
+				'post_type'   => 'attachment',
 				'numberposts' => 1,
 				'post_status' => null,
 				'post_parent' => $product['ID'],
-				'orderby' => 'menu_order',
-				'order' => 'ASC'
-			));
+				'orderby'     => 'menu_order',
+				'order'       => 'ASC'
+			) );
 			$attached_image = $attached_images[0]; 
-			if(($attached_image->ID > 0)) {
-					$output .= "	<img src='". wpsc_product_image($attached_image->ID, get_option('product_image_width'), get_option('product_image_height'))."' title='".$product['post_title']."' alt='".$product['post_title']."' /><br />";
+			if ( ( $attached_image->ID > 0 ) ) {
+				$output .= "<img src='" . wpsc_product_image( $attached_image->ID, get_option( 'product_image_width' ), get_option( 'product_image_height' ) ) . "' title='" . $product['post_title'] . "' alt='" . $product['post_title'] . "' /><br />";
 			}
-			//exit('<pre>'.print_r($image,true).'</pre>');
-			$output .= "<strong>".$product['post_title']."</strong><br />";
-		
-			$output .= $product['post_content']."<br />";
-		
-			$output .= "<form name='".$product['ID']."' method='post' action='' >";
-			$output .= "<input type='hidden' name='product_id' value='".$product['ID']."'/>";
-			$output .= "<input type='hidden' name='item' value='".$product['ID']."' />";
+			
+			// Get currency options
+			$currency_sign_location = get_option( 'currency_sign_location' );
+			$currency_type = get_option( 'currency_type' );
+			$currency_symbol = $wpdb->get_var( "SELECT `symbol_html` FROM `" . WPSC_TABLE_CURRENCY_LIST . "` WHERE `id`='" . $currency_type . "' LIMIT 1" );
+			
+			// Output
+			$output .= "<strong>" . $product['post_title'] . "</strong><br />";
+			$output .= $product['post_content'] . "<br />";
+			$output .= "<form name='" . $product['ID'] . "' method='post' action=''>";
+			$output .= "<input type='hidden' name='product_id' value='" . $product['ID'] . "'/>";
+			$output .= "<input type='hidden' name='item' value='" . $product['ID'] . "' />";
 			$output .= "<input type='hidden' name='wpsc_ajax_action' value='donations_widget' />";		
-			$currency_sign_location = get_option('currency_sign_location');
-			$currency_type = get_option('currency_type');
-			$currency_symbol = $wpdb->get_var("SELECT `symbol_html` FROM `".WPSC_TABLE_CURRENCY_LIST."` WHERE `id`='".$currency_type."' LIMIT 1") ;
-			$output .= "<label for='donation_widget_price_".$product['ID']."'>".__('Donation', 'wpsc').":</label> $currency_symbol<input type='text' id='donation_widget_price_".$product['ID']."' name='donation_price' value='".number_format($product['price'],2)."' size='6' /><br />"; 
-			$output .= "<input type='submit' name='Buy' value='".__('Add To Cart', 'wpsc')."'  />";
+			$output .= "<label for='donation_widget_price_" . $product['ID'] . "'>" . __( 'Donation', 'wpsc' ) . ":</label> $currency_symbol<input type='text' id='donation_widget_price_" . $product['ID'] . "' name='donation_price' value='" . number_format( $product['price'], 2 ) . "' size='6' /><br />";
+			$output .= "<input type='submit' name='Buy' value='" . __( 'Add To Cart', 'wpsc' ) . "' />";
 			$output .= "</form>";
+			
 		}
 		$output .= "</div></div>";
 	} else {
 		$output = '';
 	}
-	echo $input.$output;
-}
-
-/*
- * Specials Widget control function
- * Displays the products
- */
-function widget_donations_control() { 
-  $option_name = 'wpsc-nzshpcrt_donations';  // because I want to only change this to reuse the code.
-	$options = $newoptions = get_option($option_name);
-	if ( isset($_POST[$option_name]) ) {
-		$newoptions['title'] = strip_tags(stripslashes($_POST[$option_name]));
-	}
-	if ( $options != $newoptions ) {
-		$options = $newoptions;
-		update_option($option_name, $options);
-	}
-	$title = htmlspecialchars($options['title'], ENT_QUOTES);
 	
-	echo "<p>\n\r";
-	echo "  <label for='{$option_name}'>"._e('Title:')."<input class='widefat' id='{$option_name}' name='{$option_name}' type='text' value='{$title}' /></label>\n\r";
-	echo "</p>\n\r";
+	echo $output;
+	
 }
 
 
-/*
- * Specials Widget init function
- * Displays the products
- */
-function widget_donations_init() {
-  if(function_exists('wp_register_sidebar_widget')) {
-    wp_register_sidebar_widget('widget_donations', __('Product Donations', 'wpsc'), 'widget_donations');
-    wp_register_widget_control('widget_donations', __('Product Donations', 'wpsc'), 'widget_donations_control');
-	}
-  return;
-}
 
-add_action('plugins_loaded', 'widget_donations_init');
 ?>
