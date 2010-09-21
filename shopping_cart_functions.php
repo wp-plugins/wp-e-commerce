20501d2ff3e616d45e1e337e6f295e24aa81ac1c
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-21 20:14:08 +0000

    Fix code formatting in shopping_cart_functions.php
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@292170 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shopping_cart_functions.php
+++ shopping_cart_functions.php
@@ -1,68 +1,75 @@
 <?php
-function wpsc_shopping_cart($input = null, $override_state = null) {
-  global $wpdb;  
-  if(is_numeric($override_state)) {
-    $state = $override_state;
-	} else {
-		$state = get_option('cart_location');
-	}
 
-	
-	if(get_option('show_sliding_cart') == 1)	{
-		if(isset($_SESSION['slider_state']) && is_numeric($_SESSION['slider_state']))	{
-		if($_SESSION['slider_state'] == 0) { $collapser_image = 'plus.png'; } else { $collapser_image = 'minus.png'; }
-			$fancy_collapser = "<a href='#' onclick='return shopping_cart_collapser()' id='fancy_collapser_link'><img src='".WPSC_URL."/images/$collapser_image' title='' alt='' id='fancy_collapser' /></a>";
+function wpsc_shopping_cart( $input = null, $override_state = null ) {
+	global $wpdb;
+
+	if ( is_numeric( $override_state ) )
+		$state = $override_state;
+	else
+		$state = get_option( 'cart_location' );
+
+	if ( get_option( 'show_sliding_cart' ) == 1 ) {
+		if ( isset( $_SESSION['slider_state'] ) && is_numeric( $_SESSION['slider_state'] ) ) {
+			if ( $_SESSION['slider_state'] == 0 ) {
+				$collapser_image = 'plus.png';
+			} else {
+				$collapser_image = 'minus.png';
+			}
+			$fancy_collapser = "<a href='#' onclick='return shopping_cart_collapser()' id='fancy_collapser_link'><img src='" . WPSC_URL . "/images/$collapser_image' title='' alt='' id='fancy_collapser' /></a>";
 		} else {
-			if(isset($_SESSION['nzshpcrt_cart'])) { $collapser_image = 'minus.png';} else { $collapser_image = 'plus.png'; }
-			$fancy_collapser = "<a href='#' onclick='return shopping_cart_collapser()' id='fancy_collapser_link'><img src='".WPSC_URL."/images/$collapser_image' title='' alt='' id='fancy_collapser' /></a>";
+			if ( isset( $_SESSION['nzshpcrt_cart'] ) ) {
+				$collapser_image = 'minus.png';
+			} else {
+				$collapser_image = 'plus.png';
+			}
+			$fancy_collapser = "<a href='#' onclick='return shopping_cart_collapser()' id='fancy_collapser_link'><img src='" . WPSC_URL . "/images/$collapser_image' title='' alt='' id='fancy_collapser' /></a>";
 		}
 	} else {
 		$fancy_collapser = "";
 	}
 
-  
-  if($state == 1) {
-    if($input != '') {
-      $cart = $_SESSION['nzshpcrt_cart'];
-      echo "<div id='sideshoppingcart'><div id='shoppingcartcontents'>";
-      echo wpsc_shopping_basket_internals($cart);
-      echo "</div></div>";
+	if ( $state == 1 ) {
+		if ( $input != '' ) {
+			$cart = $_SESSION['nzshpcrt_cart'];
+			echo "<div id='sideshoppingcart'><div id='shoppingcartcontents'>";
+			echo wpsc_shopping_basket_internals( $cart );
+			echo "</div></div>";
 		}
-  } else if(($state == 3) || ($state == 4)) {
-		if (isset($_SESSION['nzshpcrt_cart'])) {
+	} else if ( ($state == 3) || ($state == 4) ) {
+		if ( isset( $_SESSION['nzshpcrt_cart'] ) ) {
 			$cart = $_SESSION['nzshpcrt_cart'];
 		}
-		if($state == 4) {
+		if ( $state == 4 ) {
 			#echo $input;
 			echo "<div id='widgetshoppingcart'>";
-			echo "<h3>".__('Shopping Cart')."$fancy_collapser</h3>";
+			echo "<h3>" . __( 'Shopping Cart' ) . "$fancy_collapser</h3>";
 			echo "  <div id='shoppingcartcontents'>";
-				echo wpsc_shopping_basket_internals($cart,false,true);
+			echo wpsc_shopping_basket_internals( $cart, false, true );
 			echo "  </div>";
 			echo "</div>";
 			$dont_add_input = true;
 		} else {
 			echo "<div id='sideshoppingcart'>";
-			echo "<h3>".__('Shopping Cart')."$fancy_collapser</h3>";
+			echo "<h3>" . __( 'Shopping Cart' ) . "$fancy_collapser</h3>";
 			echo "  <div id='shoppingcartcontents'>";
-			if (isset($cart)) {
-				echo wpsc_shopping_basket_internals($cart,false,true);
+			if ( isset( $cart ) ) {
+				echo wpsc_shopping_basket_internals( $cart, false, true );
 			}
 			echo "  </div>";
 			echo "</div>";
 		}
 	} else {
-		if((isset($GLOBALS['nzshpcrt_activateshpcrt']) && $GLOBALS['nzshpcrt_activateshpcrt'] === true)) {
-		
+		if ( (isset( $GLOBALS['nzshpcrt_activateshpcrt'] ) && $GLOBALS['nzshpcrt_activateshpcrt'] === true ) ) {
+
 			$cart = $GLOBALS['nzshpcrt_activateshpcrt'];
-			
-			if(isset($_SESSION['nzshpcrt_cart'])) {
+
+			if ( isset( $_SESSION['nzshpcrt_cart'] ) ) {
 				$cart = $_SESSION['nzshpcrt_cart'];
 			}
 			echo "<div id='shoppingcart'>";
-			echo "<h3>".__('Shopping Cart')."$fancy_collapser</h3>";
+			echo "<h3>" . __( 'Shopping Cart' ) . "$fancy_collapser</h3>";
 			echo "  <div id='shoppingcartcontents'>";
-			echo wpsc_shopping_basket_internals($cart,false,true);
+			echo wpsc_shopping_basket_internals( $cart, false, true );
 			echo "  </div>";
 			echo "</div>";
 		}
@@ -71,96 +78,102 @@ function wpsc_shopping_cart($input = null, $override_state = null) {
 }
 
 // preserved for backwards compatibility
-function nzshpcrt_shopping_basket($input = null, $override_state = null) {
-  return wpsc_shopping_cart($input, $override_state);
+function nzshpcrt_shopping_basket( $input = null, $override_state = null ) {
+	return wpsc_shopping_cart( $input, $override_state );
 }
 
-function wpsc_shopping_basket_internals($cart,$quantity_limit = false, $no_title=false) {
-  global $wpdb, $wpsc_theme_path;
+function wpsc_shopping_basket_internals( $cart, $quantity_limit = false, $no_title=false ) {
+	global $wpdb, $wpsc_theme_path;
 	$display_state = "";
-	if((( (isset($_SESSION['slider_state']) && $_SESSION['slider_state'] == 0) )|| (wpsc_cart_item_count() < 1)) && (get_option('show_sliding_cart') == 1)) {
+	if ( (( (isset( $_SESSION['slider_state'] ) && $_SESSION['slider_state'] == 0) ) || (wpsc_cart_item_count() < 1)) && (get_option( 'show_sliding_cart' ) == 1) ) {
 		$display_state = "style='display: none;'";
 	}
 	echo "    <div id='sliding_cart' class='shopping-cart-wrapper' $display_state>";
 //	$cur_wpsc_theme_folder = apply_filters('wpsc_theme_folder',$wpsc_theme_path.WPSC_THEME_DIR);
-	$cur_wpsc_theme_folder = apply_filters('wpsc_theme_folder',$wpsc_theme_path);
-	include_once($cur_wpsc_theme_folder."/wpsc-cart_widget.php");
+	$cur_wpsc_theme_folder = apply_filters( 'wpsc_theme_folder', $wpsc_theme_path );
+	include_once($cur_wpsc_theme_folder . "/wpsc-cart_widget.php");
 	echo "    </div>";
- return $output;
+	return $output;
 }
-  
-function wpsc_country_region_list($form_id = null, $ajax = false , $selected_country = null, $selected_region = null, $supplied_form_id = null, $checkoutfields = false) {
-  global $wpdb;
-  $output = '';
-  if($selected_country == null) {
-    $selected_country = get_option('base_country');
-	}
-  if($selected_region == null) {
-    $selected_region = get_option('base_region');
-	}
-  if($form_id != null) {
-    $html_form_id = "region_country_form_$form_id";
-	} else {
+
+function wpsc_country_region_list( $form_id = null, $ajax = false, $selected_country = null, $selected_region = null, $supplied_form_id = null, $checkoutfields = false ) {
+	global $wpdb;
+	$output = '';
+
+	if ( $selected_country == null )
+		$selected_country = get_option( 'base_country' );
+
+	if ( $selected_region == null )
+		$selected_region = get_option( 'base_region' );
+
+	if ( $form_id != null )
+		$html_form_id = "region_country_form_$form_id";
+	else
 		$html_form_id = 'region_country_form';
-	}
-	if($supplied_form_id != null) {
-	  $supplied_form_id = "id='$supplied_form_id'";
-	}
-	if($checkoutfields){
+
+	if ( $supplied_form_id != null )
+		$supplied_form_id = "id='$supplied_form_id'";
+
+	if ( $checkoutfields ) {
 		$js = "onchange='set_shipping_country(\"$html_form_id\", \"$form_id\");'";
 		$title = 'shippingcountry';
-	}else{
-	
-		$js= "onchange='set_billing_country(\"$html_form_id\", \"$form_id\");'";
+	} else {
+		$js = "onchange='set_billing_country(\"$html_form_id\", \"$form_id\");'";
 		$title = 'billingcountry';
 	}
-  $country_data = $wpdb->get_results("SELECT * FROM `".WPSC_TABLE_CURRENCY_LIST."` ORDER BY `country` ASC",ARRAY_A);
-  $output .= "<div id='$html_form_id'>\n\r";
-  $output .= "<select $supplied_form_id title='$title' name='collected_data[".$form_id."][0]' class='current_country' $js >\n\r";
-  foreach ($country_data as $country) {
-    $selected ='';
-   if($country['visible'] == '1'){
-  
-		if($selected_country == $country['isocode']) {
-		  $selected = "selected='selected'";
+
+	$country_data = $wpdb->get_results( "SELECT * FROM `" . WPSC_TABLE_CURRENCY_LIST . "` ORDER BY `country` ASC", ARRAY_A );
+	$output .= "<div id='$html_form_id'>\n\r";
+	$output .= "<select $supplied_form_id title='$title' name='collected_data[" . $form_id . "][0]' class='current_country' $js >\n\r";
+
+	foreach ( $country_data as $country ) {
+		$selected = '';
+		if ( $country['visible'] == '1' ) {
+			if ( $selected_country == $country['isocode'] ) {
+				$selected = "selected='selected'";
 			}
-		$output .= "<option value='".$country['isocode']."' $selected>".htmlentities($country['country'])."</option>\n\r";
-		}  
+			$output .= "<option value='" . $country['isocode'] . "' $selected>" . htmlentities( $country['country'] ) . "</option>\n\r";
+		}
 	}
 
-  $output .= "</select>\n\r";
-  
-  
-  $region_list = $wpdb->get_results("SELECT `".WPSC_TABLE_REGION_TAX."`.* FROM `".WPSC_TABLE_REGION_TAX."`, `".WPSC_TABLE_CURRENCY_LIST."`  WHERE `".WPSC_TABLE_CURRENCY_LIST."`.`isocode` IN('".$selected_country."') AND `".WPSC_TABLE_CURRENCY_LIST."`.`id` = `".WPSC_TABLE_REGION_TAX."`.`country_id`",ARRAY_A) ;
-  $sql ="SELECT `".WPSC_TABLE_CHECKOUT_FORMS."`.`id` FROM `".WPSC_TABLE_CHECKOUT_FORMS."` WHERE `unique_name` = 'shippingstate' ";
-  $region_form_id = $wpdb->get_var($sql);
-  if($checkoutfields){
-  	$namevalue = "name='collected_data[".$region_form_id."]'"; 
-//  	$namevalue = "name='collected_data[".$form_id."][1]'"; 
-	$js = "onchange='set_shipping_country(\"$html_form_id\", \"$form_id\");'";
-	$title = 'shippingregion';
-  }else{
-  	$namevalue = "name='collected_data[".$form_id."][1]'"; 
-  	$js= "onchange='set_billing_country(\"$html_form_id\", \"$form_id\");'";
-  	$title = 'billingregion';
-  }
- // exit('Not here? >'.$region_form_id.' ' .$sql);
-    $output .= "<div id='region_select_$form_id'>";
-    if($region_list != null) {
-      $output .= "<select title='$title' ".$namevalue." class='current_region' ".$js.">\n\r";
-      //$output .= "<option value=''>None</option>";
-      foreach($region_list as $region) {
-        if($selected_region == $region['id']) {
-          $selected = "selected='selected'";
-				} else {
-					$selected = "";
-				}
-        $output .= "<option value='".$region['id']."' $selected>".htmlentities($region['name'])."</option>\n\r";
+	$output .= "</select>\n\r";
+
+
+	$region_list    = $wpdb->get_results( "SELECT `" . WPSC_TABLE_REGION_TAX . "`.* FROM `" . WPSC_TABLE_REGION_TAX . "`, `" . WPSC_TABLE_CURRENCY_LIST . "`  WHERE `" . WPSC_TABLE_CURRENCY_LIST . "`.`isocode` IN('" . $selected_country . "') AND `" . WPSC_TABLE_CURRENCY_LIST . "`.`id` = `" . WPSC_TABLE_REGION_TAX . "`.`country_id`", ARRAY_A );
+	$sql            = "SELECT `" . WPSC_TABLE_CHECKOUT_FORMS . "`.`id` FROM `" . WPSC_TABLE_CHECKOUT_FORMS . "` WHERE `unique_name` = 'shippingstate' ";
+	$region_form_id = $wpdb->get_var( $sql );
+
+	if ( $checkoutfields ) {
+		$namevalue = "name='collected_data[" . $region_form_id . "]'";
+//  	$namevalue = "name='collected_data[".$form_id."][1]'";
+		$js = "onchange='set_shipping_country(\"$html_form_id\", \"$form_id\");'";
+		$title = 'shippingregion';
+	} else {
+		$namevalue = "name='collected_data[" . $form_id . "][1]'";
+		$js = "onchange='set_billing_country(\"$html_form_id\", \"$form_id\");'";
+		$title = 'billingregion';
+	}
+
+	// exit('Not here? >'.$region_form_id.' ' .$sql);
+	$output .= "<div id='region_select_$form_id'>";
+	if ( $region_list != null ) {
+		$output .= "<select title='$title' " . $namevalue . " class='current_region' " . $js . ">\n\r";
+		//$output .= "<option value=''>None</option>";
+		foreach ( $region_list as $region ) {
+			if ( $selected_region == $region['id'] ) {
+				$selected = "selected='selected'";
+			} else {
+				$selected = "";
 			}
-      $output .= "</select>\n\r";
+			$output .= "<option value='" . $region['id'] . "' $selected>" . htmlentities( $region['name'] ) . "</option>\n\r";
 		}
-  $output .= "</div>";
-  $output .= "</div>\n\r";
-  return $output;
+		$output .= "</select>\n\r";
+	}
+
+	$output .= "</div>";
+	$output .= "</div>\n\r";
+
+	return $output;
 }
+
 ?>
