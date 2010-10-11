60d49b1631c3ef1fc418d651793a9114e5bac942
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-11 05:11:11 +0000

    Hmmms 2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@298788 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-admin/display-coupons.php
+++ wpsc-admin/display-coupons.php
@@ -138,7 +138,7 @@ function wpsc_display_coupons_page() {
 	<div class="wrap">
 		<h2>
 			<?php _e( 'Coupons', 'wpsc' ); ?>
-			<a href="" id="add_coupon_box_link" class="add_item_link button add-new-h2" onClick="return show_status_box( 'add_coupon_box', 'add_coupon_box_link' );">
+			<a href="#" id="add_coupon_box_link" class="add_item_link button add-new-h2" onClick="return show_status_box( 'add_coupon_box', 'add_coupon_box_link' );">
 				<?php _e( 'Add New', 'wpsc' ); ?>
 			</a>
 		</h2>
