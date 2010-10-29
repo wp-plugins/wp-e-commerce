98f567fa0f67dfe7dcc8066777dbbc65f20f6974
valentinas <valentinas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-29 03:56:15 +0000

    cart widget styling
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@305591 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-theme/wpsc-cart_widget.php
+++ wpsc-theme/wpsc-cart_widget.php
@@ -8,21 +8,20 @@
 <?php } ?>
 
 <?php if(wpsc_cart_item_count() > 0): ?>
-		<span class="numberitems">
-			<?php _e('Number of items', 'wpsc'); ?>:
-			<?php echo wpsc_cart_item_count(); ?>
-		</span>
     <div class="shoppingcart">
 	<table>
-		<tr>
-			<th id="product"><?php _e('Product', 'wpsc'); ?></th>
-			<th id="quantity"><?php _e('Qty', 'wpsc'); ?></th>
-			<th id="price"><?php _e('Price', 'wpsc'); ?></th>
-            <th id="remove">&nbsp;</th>
-		</tr>
+		<thead>
+			<tr>
+				<th id="product" colspan='2'><?php _e('Product', 'wpsc'); ?></th>
+				<th id="quantity"><?php _e('Qty', 'wpsc'); ?></th>
+				<th id="price"><?php _e('Price', 'wpsc'); ?></th>
+	            <th id="remove">&nbsp;</th>
+			</tr>
+		</thead>
+		<tbody>
 		<?php while(wpsc_have_cart_items()): wpsc_the_cart_item(); ?>
 			<tr>
-					<td><?php echo wpsc_cart_item_name(); ?></td>
+					<td colspan='2' class='product-name'><?php echo wpsc_cart_item_name(); ?></td>
 					<td><?php echo wpsc_cart_item_quantity(); ?></td>
 					<td><?php echo wpsc_cart_item_price(); ?></td>
                     <td><form action="" method="post" class="adjustform">
@@ -33,32 +32,50 @@
 				</form></td>
 			</tr>	
 		<?php endwhile; ?>
-	</table>
-	</div><!--close shoppingcart-->
-<?php if(wpsc_cart_has_shipping() && !wpsc_cart_show_plus_postage()) : ?>
-		  <span class="pricedisplay checkout-shipping"><?php _e('Shipping', 'wpsc'); ?>: <?php echo wpsc_cart_shipping(); ?></span>
-	<?php endif; ?>
-<?php if( (wpsc_cart_tax(false) >0) && !wpsc_cart_show_plus_postage()) : ?>
-		  <span class="pricedisplay checkout-tax"><?php echo wpsc_display_tax_label(true); ?>: <?php echo wpsc_cart_tax(); ?></span>			
-	<?php endif; ?>
-		<span class="pricedisplay checkout-total">
-			<?php _e('Total', 'wpsc'); ?>: <?php echo wpsc_cart_total_widget(); ?>
+		<tbody>
+		<tfoot>
+			<tr class="cart-widget-total">
+				<td>
+					<?php printf( _n('%d item', '%d items', wpsc_cart_item_count(), 'wpsc'), wpsc_cart_item_count() ); ?>
+				</td>
+				<td class="pricedisplay checkout-total" colspan='4'>
+					<?php _e('Total', 'wpsc'); ?>: <?php echo wpsc_cart_total_widget(); ?>
+				</td>
+			</tr>
 			<?php if(wpsc_cart_show_plus_postage()) : ?>
-				<span class="pluspostagetax"> + <?php _e('Postage &amp; Tax ', 'wpsc'); ?></span>
+			<tr>
+				<td class="pluspostagetax" colspan='5'>
+					+ <?php _e('Postage &amp; Tax ', 'wpsc'); ?>
+				</td>
+			</tr>
 			<?php endif; ?>
-		</span>	
-
-	
-	<a target="_parent" href="<?php echo get_option('shopping_cart_url'); ?>" title="Checkout" class="gocheckout"><?php _e('Checkout', 'wpsc'); ?></a>
+			<?php if(wpsc_cart_has_shipping() && !wpsc_cart_show_plus_postage()) : ?>
+			<tr>
+				<td class="pricedisplay checkout-shipping" colspan='5'>
+					<?php _e('Shipping', 'wpsc'); ?>: <?php echo wpsc_cart_shipping(); ?>
+				</td>
+			</tr>
+			<?php endif; ?>
+			<?php if( (wpsc_cart_tax(false) >0) && !wpsc_cart_show_plus_postage()) : ?>
+			<tr>
+				<td class="pricedisplay checkout-tax" colspan='5'>
+					<?php echo wpsc_display_tax_label(true); ?>: <?php echo wpsc_cart_tax(); ?>
+				</td>
+			</tr>                
+			<?php endif; ?>
+		</tfoot>
+	</table>
+	</div><!--close shoppingcart-->		
+	<div id='cart-widget-links'>
+		<a target="_parent" href="<?php echo get_option('shopping_cart_url'); ?>" title="Checkout" class="gocheckout"><?php _e('Checkout', 'wpsc'); ?></a>
+		<form action="" method="post" class="wpsc_empty_the_cart">
+			<input type="hidden" name="wpsc_ajax_action" value="empty_cart" />
+				<a target="_parent" href="<?php echo htmlentities(add_query_arg('wpsc_ajax_action', 'empty_cart', remove_query_arg('ajax')), ENT_QUOTES); ?>" class="emptycart" title="Empty Your Cart"><?php _e('Clear cart', 'wpsc'); ?></a>                                                                                    
+		</form>
+	</div>
 <?php else: ?>
 	<p class="empty"><?php _e('Your shopping cart is empty', 'wpsc'); ?></p>
 	  <a target="_parent" href="<?php echo get_option('product_list_url'); ?>" class="visitshop" title="Visit Shop"><?php _e('Visit the shop', 'wpsc'); ?></a>
-<?php endif; ?>
-<?php if (wpsc_have_cart_items()) : ?>
-	<form action="" method="post" class="wpsc_empty_the_cart">
-		<input type="hidden" name="wpsc_ajax_action" value="empty_cart" />
-			<a target="_parent" href="<?php echo htmlentities(add_query_arg('wpsc_ajax_action', 'empty_cart', remove_query_arg('ajax')), ENT_QUOTES); ?>" class="emptycart" title="Empty Your Cart"><?php _e('Empty your cart', 'wpsc'); ?></a>                                                                                    
-	</form>
 <?php endif; ?>
 
 <?php
