dbe5718e09da6f6b525bde95a908a0e1a8270782
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-10 04:17:18 +0000

    Added vissers fixes to widgets
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@310005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-widgets/category_widget.php
+++ wpsc-widgets/category_widget.php
@@ -12,7 +12,10 @@ class WP_Widget_Product_Categories extends WP_Widget {
 	 * Widget Constuctor
 	 */
 	function WP_Widget_Product_Categories() {
-		$widget_ops = array( 'classname' => 'widget_wpsc_categorisation', 'description' => __( 'Product Grouping Widget', 'wpsc' ) );
+		$widget_ops = array(
+			'classname' => 'widget_wpsc_categorisation',
+			'description' => __( 'Product Grouping Widget', 'wpsc' )
+		);
 		$this->WP_Widget( 'wpsc_categorisation', __( 'Product Categories', 'wpsc' ), $widget_ops );
 	}
 
@@ -98,7 +101,11 @@ class WP_Widget_Product_Categories extends WP_Widget {
 		global $wpdb;
 
 		// Defaults
-		$instance = wp_parse_args((array) $instance, array( 'title' => '' ));
+		$instance = wp_parse_args((array) $instance, array(
+			'title' => '',
+			'width' => 45,
+			'height' => 45
+		));
 
 		// Values
 		$title    = esc_attr( $instance['title'] );
@@ -111,6 +118,7 @@ class WP_Widget_Product_Categories extends WP_Widget {
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
 		</p>
+
 		<p>
 			Show Categories:<br />
 			<?php wpsc_list_categories('wpsc_category_widget_admin_category_list', array("id"=>$this->get_field_id('categories'),"name"=>$this->get_field_name('categories'),"instance"=>$instance), 0); ?>
@@ -122,12 +130,11 @@ class WP_Widget_Product_Categories extends WP_Widget {
 			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('image'); ?>" name="<?php echo $this->get_field_name('image'); ?>"<?php checked( $image ); ?> onclick="jQuery('.wpsc_category_image').toggle()" />
 			<label for="<?php echo $this->get_field_id('image'); ?>"><?php _e('Show Thumbnails', 'wpsc'); ?></label>
 		</p>
+
 		<div class="wpsc_category_image"<?php if( !checked( $image ) ) { echo ' style="display:none;"'; } ?>>
 			<p>
 				<label for="<?php echo $this->get_field_id('width'); ?>"><?php _e('Width:', 'wpsc'); ?></label>
 				<input type="text" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" value="<?php echo $width ; ?>" size="3" />
-			</p>
-			<p>
 				<label for="<?php echo $this->get_field_id('height'); ?>"><?php _e('Height:', 'wpsc'); ?></label>
 				<input type="text" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" value="<?php echo $height ; ?>" size="3" />
 			</p>
--- wpsc-widgets/latest_product_widget.php
+++ wpsc-widgets/latest_product_widget.php
@@ -49,9 +49,11 @@ class WP_Widget_Latest_Products extends WP_Widget {
 	function update( $new_instance, $old_instance ) {
 
 		$instance = $old_instance;
-		$instance['title']  = strip_tags( $new_instance['title'] );
-		$instance['number'] = (int)$new_instance['number'];
-		$instance['show_thumbnails'] = (bool)$new_instance['show_thumbnails'];
+		$instance['title']      = strip_tags( $new_instance['title'] );
+		$instance['number']     = (int)$new_instance['number'];
+		$instance['image']      = (bool)$new_instance['image'];
+		$instance['height']     = (int)$new_instance['height'];
+		$instance['width']      = (int)$new_instance['width'];
 
 		return $instance;
 
@@ -67,39 +69,63 @@ class WP_Widget_Latest_Products extends WP_Widget {
 		global $wpdb;
 
 		// Defaults
-		$instance = wp_parse_args( (array)$instance, array( 'title' => '', 'number' => 5 ) );
+		$instance = wp_parse_args( (array)$instance, array(
+			'title' => '',
+			'number' => 5,
+			'width' => 45,
+			'height' => 45
+		) );
 
 		// Values
-		$title  = esc_attr( $instance['title'] );
-		$number = (int)$instance['number'];
-		$show_thumbnails = (bool)$instance['show_thumbnails']; ?>
+		$title    = esc_attr( $instance['title'] );
+		$number   = (int)$instance['number'];
+		$image    = (bool)$instance['image'];
+		$width    = (int) $instance['width'];
+		$height   = (int) $instance['height']; ?>
 
 		<p>
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
 		</p>
+
 		<p>
 			<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of products to show:', 'wpsc' ); ?></label>
 			<input type="text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" value="<?php echo $number; ?>" size="3" />
 		</p>
 
 		<p>
-			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'show_thumbnails' ); ?>" name="<?php echo $this->get_field_name( 'show_thumbnails' ); ?>" <?php echo $show_thumbnails ? 'checked="checked"' : ""; ?>>
-			<label for="<?php echo $this->get_field_id( 'show_thumbnails' ); ?>"><?php _e( 'Show Thumbnails', 'wpsc' ); ?></label>
+			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>"<?php checked($image); ?> onclick="jQuery('.wpsc_latest_image').toggle()">
+			<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php _e( 'Show Thumbnails', 'wpsc' ); ?></label>
 		</p>
+
+		<div class="wpsc_latest_image"<?php if( !checked( $image ) ) { echo ' style="display:none;"'; } ?>>
+			<p>
+				<label for="<?php echo $this->get_field_id('width'); ?>"><?php _e('Width:', 'wpsc'); ?></label>
+				<input type="text" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" value="<?php echo $width ; ?>" size="3" />
+				<label for="<?php echo $this->get_field_id('height'); ?>"><?php _e('Height:', 'wpsc'); ?></label>
+				<input type="text" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" value="<?php echo $height ; ?>" size="3" />
+			</p>
+		</div>
 <?php
 	}
 }
 
 add_action( 'widgets_init', create_function( '', 'return register_widget("WP_Widget_Latest_Products");' ) );
 
-function wpsc_latest_product() {
+function wpsc_latest_product( $args = null, $instance ) {
 	global $wpdb;
 	$args = wp_parse_args( (array)$args, array( 'number' => 5 ) );
 	$siteurl = get_option( 'siteurl' );
 	$options = get_option( 'wpsc-widget_latest_products' );
 	$number  = isset($instance['number']) ? (int)$instance['number'] : 5;
-	$show_thumbnails  = isset($instance['show_thumbnails']) ? (bool)$instance['show_thumbnails'] : FALSE;
+	$image  = isset($instance['image']) ? (bool)$instance['image'] : FALSE;
+
+	if ( isset($instance['width'] ) )
+		$width = $instance['width'];
+
+	if ( isset( $instance['height'] ) )
+		$height = $instance['height'];
+
 	$latest_products = get_posts( array(
 		'post_type'   => 'wpsc-product',
 		'numberposts' => $number, 
@@ -111,11 +137,11 @@ function wpsc_latest_product() {
 	$output = '';
 
 	if ( count( $latest_products ) > 0 ) {
-		$output .= '<ul class="wpec-latest-products">';		
+		$output .= '<ul class="wpsc-latest-products">';		
 		foreach ( $latest_products as $latest_product ) {
-			$output .= '<li class="wpec-latest-product">';
+			$output .= '<li class="wpsc-latest-product">';
 			// Thumbnails, if required
-			if (!$show_thumbnails) {
+			if ($image) {
 				$output .= '<div class="item_image">';
 				$output .= '<a href="' . wpsc_product_url( $latest_product->ID, null ) . '">';
 				$attached_images = (array)get_posts( array(
@@ -128,15 +154,15 @@ function wpsc_latest_product() {
 				) );
 				$attached_image = $attached_images[0]; 
 				if ( $attached_image->ID > 0 )
-						$output .= '<img src="' . wpsc_product_image( $attached_image->ID, get_option('product_image_width'), get_option('product_image_height') ) . '" title="' . $latest_product->post_title . '" alt="' . $latest_product->post_title . '" />';
+						$output .= '<img src="' . wpsc_product_image( $attached_image->ID, $width, $height ) . '" title="' . $latest_product->post_title . '" alt="' . $latest_product->post_title . '" />';
 				else
-					$output .='<img class="no-image" id="product_image_'.wpsc_the_product_id().'" alt="No Image" title="'.wpsc_the_product_title().'" src="'.WPSC_URL.'/wpsc-theme/wpsc-images/noimage.png" width="'. get_option('product_image_width').'" height="'.get_option('product_image_height').'" />';
+					$output .='<img class="no-image" id="product_image_'.wpsc_the_product_id().'" alt="No Image" title="'.wpsc_the_product_title().'" src="'.WPSC_URL.'/wpsc-theme/images/noimage.png" width="' . $width . '" height="' . $height . '" />';
 				
 				$output .= '</a>';
 				$output .= '</div>';
 			}
 			// Link
-			$output .= '<a href="' . wpsc_product_url( $latest_product->ID, null ) . '" class="wpec-product-title">'.stripslashes( $latest_product->post_title ).'</a>';
+			$output .= '<a href="' . wpsc_product_url( $latest_product->ID, null ) . '" class="wpsc-product-title">'.stripslashes( $latest_product->post_title ).'</a>';
 			$output .= '</li>';
 		}
 		$output .= "</ul>";
