f084ffbb951599a22b897976c690d368ef72a394
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-08 23:02:44 +0000

    Allow separate single product image
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@320729 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-includes/product-template.php
+++ wpsc-includes/product-template.php
@@ -1021,7 +1021,7 @@ function wpsc_the_product_thumbnail( $width = null, $height = null, $product_id 
 	}
 
 	// Use product thumbnail
-	if ( has_post_thumbnail( $product_id ) ) {
+	if ( has_post_thumbnail( $product_id ) && 'single' != $page) {
 		$thumbnail_id = get_post_thumbnail_id( $product_id  );
 	// Use first product image
 	} else {
