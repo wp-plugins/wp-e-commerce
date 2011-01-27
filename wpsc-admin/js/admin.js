b49056857b0ed0fe267e92938ec87d1bc080528e
valentinas <valentinas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-27 23:28:17 +0000

    fadeout fadein
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@338003 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-admin/js/admin.js
+++ wpsc-admin/js/admin.js
@@ -16,10 +16,11 @@ jQuery(document).ready(function(){
 		name = jQuery('input#title').val();
 		product_id = jQuery('input#product_id').val();
 		post_values = edit_var_val+'&description='+description+'&additional_description='+additional_description+'&name='+name+'&product_id='+product_id;
-		
-		jQuery.post('index.php?wpsc_admin_action=wpsc_update_variations',post_values, function(returned_data){
-			jQuery('div#wpsc_product_variation_forms table.widefat tbody').html(returned_data);
-			jQuery('img.loading').hide();
+		jQuery('div#wpsc_product_variation_forms table.widefat tbody').fadeTo(200, 0, function(){
+			jQuery.post('index.php?wpsc_admin_action=wpsc_update_variations',post_values, function(returned_data){
+				jQuery('div#wpsc_product_variation_forms table.widefat tbody').html(returned_data).fadeTo(200, 1);
+				jQuery('img.loading').hide();
+			});
 		});
 		return false;
 
