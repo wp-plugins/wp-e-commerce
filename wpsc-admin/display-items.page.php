ca7dae60b0e79924f5385c22b2f9bc2e5576c613
valentinas <valentinas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-28 02:25:11 +0000

    fix column order and hopefuly not break anything
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@338038 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-admin/display-items.page.php
+++ wpsc-admin/display-items.page.php
@@ -37,9 +37,7 @@ function wpsc_is_admin() {
  *
  */
 function wpsc_additional_column_names( $columns ){
-    unset( $columns['title'] );
-    unset( $columns['date'] );
-    unset( $columns['author'] );
+	$columns = array();
     
     $columns['image'] = __('');
     $columns['title'] = __('Name');
@@ -317,6 +315,7 @@ add_action( 'restrict_manage_posts', 'wpsc_cats_restrict_manage_posts' );
 add_action( 'manage_pages_custom_column', 'wpsc_additional_column_data', 10, 2 );
 add_filter( 'manage_edit-wpsc-product_sortable_columns', 'wpsc_additional_sortable_column_names' );
 add_filter( 'manage_edit-wpsc-product_columns', 'wpsc_additional_column_names' );
+add_filter( 'manage_wpsc-product_posts_columns', 'wpsc_additional_column_names' );
 add_filter( 'posts_orderby', 'wpsc_column_sql_orderby', 10, 2 );
 
 
