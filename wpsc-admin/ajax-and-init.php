04ad00f2c5854ee5608b61a7d98fc1916a48da68
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-31 03:55:17 +0000

    Fix up the CSV Download,, make it show ALL Form fields, by Unique Name,, still needs to be refactored for ease of use
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@339035 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-admin/ajax-and-init.php
+++ wpsc-admin/ajax-and-init.php
@@ -452,71 +452,71 @@ if ( isset( $_GET['wpsc_admin_action'] ) && ($_GET['wpsc_admin_action'] == 'dupl
 }
 
 function wpsc_purchase_log_csv() {
-	global $wpdb, $user_level, $wp_rewrite, $wpsc_purchlog_statuses;
+	global $wpdb, $user_level, $wp_rewrite, $wpsc_purchlog_statuses, $wpsc_gateways;
 	get_currentuserinfo();
-	if ( ($_GET['rss_key'] == 'key') && is_numeric( $_GET['start_timestamp'] ) && is_numeric( $_GET['end_timestamp'] ) && ($user_level >= 7) ) {
-		$form_sql = "SELECT * FROM `" . WPSC_TABLE_CHECKOUT_FORMS . "` WHERE `active` = '1' AND `display_log` = '1';";
+	$count = 0;
+	if ( ($_GET['rss_key'] == 'key') && is_numeric( $_GET['start_timestamp'] ) && is_numeric( $_GET['end_timestamp'] ) && (current_user_can('edit_post')) ) {
+		$form_sql = "SELECT * FROM `" . WPSC_TABLE_CHECKOUT_FORMS . "` WHERE `active` = '1' AND `type` != 'heading' ORDER BY `checkout_order` DESC;";
 		$form_data = $wpdb->get_results( $form_sql, ARRAY_A );
 
 		$start_timestamp = $_GET['start_timestamp'];
 		$end_timestamp = $_GET['end_timestamp'];
 		$data = $wpdb->get_results( "SELECT * FROM `" . WPSC_TABLE_PURCHASE_LOGS . "` WHERE `date` BETWEEN '$start_timestamp' AND '$end_timestamp' ORDER BY `date` DESC", ARRAY_A );
-
+		$csv = 'Purchase ID, Price, Firstname, Lastname, Email, Order Status, Data, ';
 		header( 'Content-Type: text/csv' );
 		header( 'Content-Disposition: inline; filename="Purchase Log ' . date( "M-d-Y", $start_timestamp ) . ' to ' . date( "M-d-Y", $end_timestamp ) . '.csv"' );
+		$headers = "\"Purchase ID\",\"Purchase Total\","; //capture the headers
+		
+		$headers2  ="\"Payment Gateway\",";
+		$headers2 .="\"Payment Status\",\"Purchase Date\",";
+
 
 		foreach ( (array)$data as $purchase ) {
+			$form_headers = '';
 			$country_sql = "SELECT * FROM `" . WPSC_TABLE_SUBMITED_FORM_DATA . "` WHERE `log_id` = '" . $purchase['id'] . "' AND `form_id` = '" . get_option( 'country_form_field' ) . "' LIMIT 1";
 			$country_data = $wpdb->get_results( $country_sql, ARRAY_A );
 			$country = $country_data[0]['value'];
-
-			$output .= "\"" . $purchase['totalprice'] . "\",";
-
+			$output .= "\"" . $purchase['id'] . "\","; //Purchase ID
+			$output .= "\"" . $purchase['totalprice'] . "\","; //Purchase Total
 			foreach ( (array)$form_data as $form_field ) {
+				$form_headers .="\"".$form_field['unique_name']."\",";
 				$collected_data_sql = "SELECT * FROM `" . WPSC_TABLE_SUBMITED_FORM_DATA . "` WHERE `log_id` = '" . $purchase['id'] . "' AND `form_id` = '" . $form_field['id'] . "' LIMIT 1";
 				$collected_data = $wpdb->get_results( $collected_data_sql, ARRAY_A );
 				$collected_data = $collected_data[0];
-				$output .= "\"" . $collected_data['value'] . "\",";
+				$output .= "\"" . $collected_data['value'] . "\","; // get form fields
 			}
+			
+			$output .= "\"" . $wpsc_gateways[$purchase['gateway']]['display_name'] . "\","; //get gateway name
 
-			if ( get_option( 'payment_method' ) == 2 ) {
-				$gateway_name = '';
-				$nzshpcrt_gateways = nzshpcrt_get_gateways();
-
-				foreach ( $nzshpcrt_gateways as $gateway ) {
-					if ( $purchase['gateway'] != 'testmode' ) {
-						if ( $gateway['internalname'] == $purchase['gateway'] ) {
-							$gateway_name = $gateway['name'];
-						}
-					} else {
-						$gateway_name = "Manual Payment";
-					}
-				}
-				$output .= "\"" . $gateway_name . "\",";
-			}
-
-			if ( $purchase['processed'] < 1 ) {
-				$purchase['processed'] = 1;
-			}
-
+	
 			$status_name = wpsc_find_purchlog_status_name( $purchase['processed'] );
-			$output .= "\"" . $status_name . "\",";
+			
+			$output .= "\"" . $status_name . "\","; //get purchase status
+			$output .= "\"" . date( "jS M Y", $purchase['date'] ) . "\","; //date
 
-			$output .= "\"" . date( "jS M Y", $purchase['date'] ) . "\"";
-
-			$cartsql = "SELECT * FROM `" . WPSC_TABLE_CART_CONTENTS . "` WHERE `purchaseid`=" . $purchase['id'] . "";
+			$cartsql = "SELECT `prodid`, `quantity`, `name` FROM `" . WPSC_TABLE_CART_CONTENTS . "` WHERE `purchaseid`=" . $purchase['id'] . "";
 			$cart = $wpdb->get_results( $cartsql, ARRAY_A );
 
+			if($count < count($cart))
+				$count = count($cart);
+			// Go through all products in cart and display quantity and sku
 			foreach ( (array)$cart as $item ) {
-				$output .= ",";
-				$product = $wpdb->get_row( "SELECT * FROM `" . $wpdb->posts . "` WHERE `id`=" . $item['prodid'] . " LIMIT 1", ARRAY_A );
 				$skuvalue = get_product_meta($item['prodid'], 'sku', true);
-				$output .= "\"" . $item['quantity'] . " " . str_replace( '"', '\"', $product['name'] ) . $variation_list . "\"";
-				$output .= "," . $skuvalue;
+				if(empty($skuvalue)) $skuvalue = 'N/A';
+				$output .= "\"" . $item['quantity'] . " x " . str_replace( '"', '\"', $item['name'] ) . "\"";
+				$output .= "," . $skuvalue."," ;
 			}
 			$output .= "\n"; // terminates the row/line in the CSV file
 		}
-		echo $output;
+		// Get the most number of products and create a header for them
+		$headers3 = "";
+		for($i = 0; $i < $count ;$i++){
+			$headers3 .= "\"Quantity - Product Name \", \" SKU \"";
+			if($i < ($count-1))
+			$headers3 .= ",";			
+		}
+
+		echo $headers . $form_headers . $headers2 . $headers3 . "\n". $output;
 		exit();
 	}
 }
