164fbc09d8875da5dd5e62d7febb9ae13dcbd24b
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-02 01:20:49 +0000

    Fix weird category url bug for products-page : http://getshopped.org/forums/topic.php?id=17494
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@339754 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-theme/wpsc-products_page.php
+++ wpsc-theme/wpsc-products_page.php
@@ -13,7 +13,7 @@ global $wp_query;
 	  <?php if(wpsc_category_grid_view()) :?>
 			<div class="wpsc_categories wpsc_category_grid group">
 				<?php wpsc_start_category_query(array('category_group'=> get_option('wpsc_default_category'), 'show_thumbnails'=> 1)); ?>
-					<a href="<?php wpsc_print_category_url(wpsc_print_category_name());?>" class="wpsc_category_grid_item  <?php wpsc_print_category_classes_section(); ?>" title="<?php wpsc_print_category_name(); ?>">
+					<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item  <?php wpsc_print_category_classes_section(); ?>" title="<?php wpsc_print_category_name(); ?>">
 						<?php wpsc_print_category_image(get_option('category_image_width'),get_option('category_image_height')); ?>
 					</a>
 					<?php wpsc_print_subcategory("", ""); ?>
