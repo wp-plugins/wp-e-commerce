a4754bd5c55afa19df42936227a541be9f093eac
valentinas <valentinas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-08 02:34:21 +0000

    localization
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@342242 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-core/wpsc-functions.php
+++ wpsc-core/wpsc-functions.php
@@ -277,7 +277,7 @@ add_action( 'rdf_item', 'wpsc_add_product_price_to_rss' );
 function wpsc_register_post_types() {
 	global $wpsc_page_titles, $wp_rewrite;
         $labels = array(
-            'name' => _x( 'Products', 'wpsc' ),
+            'name' => _x( 'Products', 'post type', 'wpsc' ),
             'singular_name' => _x( 'Product', 'wpsc' ),
             'add_new' => _x( 'Add New', 'wpsc' ),
             'add_new_item' => __('Add New Product', 'wpsc' ),
