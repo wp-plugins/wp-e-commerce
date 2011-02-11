a8cc99e35b862227a67dd183a95c21d504ca6818
JustinSainton <JustinSainton@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-11 22:26:01 +0000

    Silly logic, tricks are for kids.
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/branches/3.8-development@344353 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-admin/admin.php
+++ wpsc-admin/admin.php
@@ -256,10 +256,10 @@ function wpsc_meta_boxes() {
 	remove_meta_box( 'wpsc-variationdiv', 'wpsc-product', 'core' );
 
 	//if a variation page do not show these metaboxes
-	if ( is_object( $post ) && $post->post_parent == 0 && $post->post_status != "inherit" ) {
+	if ( is_object( $post ) && $post->post_parent == 0 ) {
 		add_meta_box( 'wpsc_product_variation_forms', __('Variations', 'wpsc'), 'wpsc_product_variation_forms', $pagename, 'normal', 'high' );
 		add_meta_box( 'wpsc_product_external_link_forms', __('Off Site Product link', 'wpsc'), 'wpsc_product_external_link_forms', $pagename, 'normal', 'high' );
-	}else {
+	} else if( $post->post_status == "inherit"  ) {
 		remove_meta_box( 'tagsdiv-product_tag', 'wpsc-product', 'core' );
 		remove_meta_box( 'wpsc_product_external_link_forms', 'wpsc-product', 'core' );
 		remove_meta_box( 'wpsc_product_categorydiv', 'wpsc-product', 'core' );
