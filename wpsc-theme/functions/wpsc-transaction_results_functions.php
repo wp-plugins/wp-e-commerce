5713287c856367dee006e37f30b71767250c431e
garyc40 <garyc40@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-25 01:21:41 +0000

    Revert "only run the default filter if no session id is set"
    This reverts commit f5d6686d87c1a2fa614f9937491fcdd2dbb3a0e6.
    
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/trunk@494860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-theme/functions/wpsc-transaction_results_functions.php
+++ wpsc-theme/functions/wpsc-transaction_results_functions.php
@@ -55,8 +55,7 @@ function wpsc_transaction_theme() {
 			break;
 			 //default filter for other payment gateways to use 
 		   default:
-		   		if ( empty($sessionid) )
-           			$sessionid = apply_filters('wpsc_previous_selected_gateway_' . $_SESSION['wpsc_previous_selected_gateway'], '');
+           		$sessionid = apply_filters('wpsc_previous_selected_gateway_' . $_SESSION['wpsc_previous_selected_gateway'], '');
            break;
 		}
 	}
