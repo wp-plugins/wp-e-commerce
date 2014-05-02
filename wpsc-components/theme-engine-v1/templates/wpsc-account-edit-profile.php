e8f3888b939efbdfb3ed03d3d4bd47944502fe63
JustinSainton <JustinSainton@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-02 05:03:15 +0000

    Tag 3.8.14
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce/tags/3.8.14@906681 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpsc-components/theme-engine-v1/templates/wpsc-account-edit-profile.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Account > Edit Profile template.
+ *
+ * Displays the user account page.
+ *
+ * @package WPSC
+ * @since WPSC 3.8.10
+ */
+?>
+
+<form method="post">
+	<table>
+
+		<?php wpsc_display_form_fields(); ?>
+
+	</table>
+</form>
